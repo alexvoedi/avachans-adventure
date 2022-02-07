64f2af3418119fa1b59c4a8b37e55dac4c214069
Alexander Vödisch <voedisch.alexander@gmail.com>
2022-02-07 19:11:26 +0100

    feat: chapter 2

--- src/data/story.ts
+++ src/data/story.ts
@@ -32,8 +32,25 @@ export default [
   },
   {
     id: 3,
-    title: "Kapitel 2",
-    text: ``,
+    title: "Zieh dich an, Mann!",
+    text: `
+      Nach dem Aufstehen zieht man sich normalerweise erst einmal etwas an. Auch du gehörst zu diesen Menschen, zumindest manchmal. Du bist dir nicht sicher, ob du heute lieber doch nackt das Haus verlassen möchtest oder nicht. Du überlegst...
+    `,
+    question: "Was ziehst du an?",
+    choices: [
+      {
+        text: "Nichts",
+        goto: [5],
+      },
+      {
+        text: "Alte Gammelkleidung",
+        goto: [6],
+      },
+      {
+        text: "Was Frisches",
+        goto: [7],
+      },
+    ],
   },
   {
     id: 4,
@@ -44,6 +61,31 @@ export default [
       Plötzlich spürst du, dass du verdammt nochmal heftige Bauchschmerzen hast. Es ist kaum zu ertragen, aber du willst unbedingt im Bett liegen bleiben und so wird der Druck immer größer, bis du explodierst.
 
       Du hättest dir besser keinen Korken in den Popo stecken sollen!
+    `,
+  },
+  {
+    id: 5,
+    title: "Bohrmaschine",
+    text: `
+      Du bleibst also nackig, wieso auch nicht?
+
+      Als du frohlockend und nackt durch die Wohnung zu hüpfen beginnst, rutscht du plötzlich aus. Welch ein Pech, denn letzte Woche hast du ausgerechnet in dieser Ecke renoviert und die Bohrmaschine liegt noch da. Du fällst mit dem Gesucht voll in den Holzbohrer, der sich sogleich durch dein Gehirn bohrt.
+
+      Das tat weh! Du hättest dir wohl doch besser was anziehen sollen, dann wärst du vielleicht an der Türklinke hängen geblieben und hättest überlebt.
+    `,
+  },
+  {
+    id: 6,
+    title: "",
+    text: "",
+  },
+  {
+    id: 7,
+    title: "",
+    text: `
+      Du suchst dir einen frischen Pullover aus dem Schank und versuchst ihn überzuziehen. Du gibst dir zwar Mühe, aber der Pullover ist wohl beim Waschen eingelaufen, so dass du jetzt feststeckst. Verzweifelt versuchst du dich zu befreien, kommst aber nicht weiter. Dir geht langsam die Luft aus und du erstickst jämmerlich in deinem frischen Pullover.
+
+      Wenigstens hast du bei der Beerdigung frische Kleidung an!
     `,
   },
 ];
